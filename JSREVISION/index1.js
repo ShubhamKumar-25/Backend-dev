0ccc139f537e34ed185efaf514ebe0958d67ea60
Shubham <sk5989229@gmail.com>
2026-07-09 21:39:09 +0530

    re

--- JSREVISION/index1.js
+++ JSREVISION/index1.js
@@ -9,6 +9,12 @@ const user = {
     }
 }
 
+// console.log(user.name);
+// console.log(user["age"]);
+// console.log("name" in user);
+// console.log('salary' in user);
+// console.log(user.hasOwnProperty('city'));
+// console.log(delete user.age);
 // user.greet();
 // user.city = "New Delhi";
 // user.greet();
@@ -18,4 +24,32 @@ const user = {
 // for(let data in user){
 //     console.log(data);
 // }
+// for(let key in user){
+//     console.log(key, user[key]);
+// }
 
+
+const newUser = {
+    name: "Shubham Kumar",
+    age: 22,
+    email: "shubham@example.com",
+    address: {
+        street: "123 Main St",
+        city: "Patna",
+        state: "Bihar",
+        zip: "800002"
+    },
+    hobbies: ["reading", "traveling", "coding"],
+    greet: function() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old. You can reach me at ${this.email}.`);
+    }
+}
+
+for(let key in newUser){
+    if(typeof newUser[key] === 'object' && !Array.isArray(newUser[key])){
+        console.log(`${key}:`);
+        for(let subKey in newUser[key]){
+            console.log(`  ${subKey}: ${newUser[key][subKey]}`);
+        }
+    }
+}
