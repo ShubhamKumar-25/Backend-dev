be737cccd30f51f040f833136dc2aa110a42fd54
Shubham <sk5989229@gmail.com>
2026-08-15 16:26:16 +0530

    Scope

--- JSREVISION/index3.js
+++ JSREVISION/index3.js
@@ -13,10 +13,37 @@ console.log(5 == '5');
 console.log(5 === "5");
 
 
+// function dec
+greet();
+function greet(){
+    console.log("Rohan Gupta");
+    
+}
 
 
 
+// Global scope
+let name = `Rohan Gupta is a developer`;
+function greet1(){
+    console.log(name);
+}
+greet1();
 
 
+
+// function scope
+function test(){
+    let age = 22;
+    console.log(age);
+}
+// console.log(age);
+test();
+
+// Block scope
+if(true){
+    let isDev = true;
+    console.log(isDev);
+}
+//console.log(isDev);
 
 
