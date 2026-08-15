ce8ff51c1fa3594921e380519ed6e681a523b38c
Shubham <sk5989229@gmail.com>
2026-08-15 15:49:24 +0530

    Js

---
+++ JSREVISION/index3.js
@@ -0,0 +1,22 @@
+// 1. String
+// 2. Number
+// 3. BigInt
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+
+console.log(typeof null);
+console.log(typeof NaN);
+
+console.log(5 == '5');
+console.log(5 === "5");
+
+
+
+
+
+
+
+
+
