12ae15c00b274b9da3700a2efe3c96f547cd708c
Shubham <sk5989229@gmail.com>
2026-08-16 15:40:37 +0530

    Lexical scope

--- JSREVISION/index3.js
+++ JSREVISION/index3.js
@@ -94,16 +94,45 @@
 // user2.greet2();
 
 
+// const user = {
+//     userName: "Shubham",
+//     age: 22,
+//     greet: function(){
+//         console.log(this.userName);
+//         console.log(this.age);
+//     }
+// }
+// user.greet();
 
-const user = {
-    name: "Rohan Gupta",
-    age: 22,
-    greet: function(){
-        console.log(this.name);
-        console.log(this.age);
-    },
-    city: "Motihari"
+// function first(){
+//     // console.log("Shubham Gupta");
+//     second();
+// }
+
+// function second(){
+//     console.log("Rohan gupta is software developer");
+// }
+
+// first();
+
+// const name = "Rohan Gupta....";
+// function aa(){
+//     console.log(name);
+// }
+// aa();
+
+const country = "India";
+function outer(){
+    const State = "Punjab";
+    console.log(State);
+    console.log(country);
+    // console.log(city); isse hum outer function ke under access nahi kar sakte hai....
+    function inner(){ // ye inner function sabhi ko access kar sakta hai.....
+        let city = "Mohali";
+        // console.log(country);
+        // console.log(State);
+        // console.log(city);
+    }
+    inner();
 }
-user.greet();
-let res = user.city;
-console.log(res);
+outer();
