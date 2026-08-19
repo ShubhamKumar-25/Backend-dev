89b722d8492ee450794896de67673fb033fe5871
Shubham <sk5989229@gmail.com>
2026-08-20 01:57:10 +0530

    que

--- JSREVISION/index3.js
+++ JSREVISION/index3.js
@@ -245,14 +245,29 @@ outer();
 
 
 
-console.log("Start");
+// console.log("Start");
+
+// setTimeout(() => {
+//     console.log("Timeout");
+// }, 0);
+
+// Promise.resolve().then(() => {
+//     console.log("Promise");
+// });
+
+// console.log("End");
+
+
+
+
+console.log(1);
 
 setTimeout(() => {
-    console.log("Timeout");
+    console.log(2);
 }, 0);
 
 Promise.resolve().then(() => {
-    console.log("Promise");
+    console.log(3);
 });
 
-console.log("End");
+console.log(4);
