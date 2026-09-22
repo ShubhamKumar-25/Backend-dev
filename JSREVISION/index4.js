56f1298d00e894d80d734edc5d55bb83d21d2a3c
Shubham <sk5989229@gmail.com>
2026-09-23 02:14:21 +0530

    map

--- JSREVISION/index4.js
+++ JSREVISION/index4.js
@@ -61,3 +61,40 @@ let names = ["Apple", "Banana", "Mango"]
 names.forEach((name) => {
     console.log(name);
 })
+
+
+let num = [1, 2, 3, 4];
+let result = num.forEach((num1) => {
+    return num1 + 3;
+})
+console.log(result);
+
+console.log("---------------------------------------------------------------");
+
+let num2 = [1, 2, 3, 4, 5];
+let result2 = num2.map((num3) => {
+    return num3 + 2
+})
+console.log(result2);
+
+console.log("-----------------------------------------------------------------");
+
+// const prices = [100, 300, 593, 653];
+// const double = prices.map((pri) => {
+//     return pri * 1.10;
+// })
+// console.log(double);
+
+
+const prices = [100, 200, 300];
+const newPrices = prices.map(price => +(price * 1.10).toFixed(2));
+console.log(newPrices);
+
+console.log("====================================================");
+let aa = [20, 34, 89, 10, 55];
+let bb = aa.filter((nn) => {
+    return nn >= 40;
+})
+console.log(bb);
+
+console.log("=======================================================");
